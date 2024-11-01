cc375dc8af6236c7a86764820d0ec8d587393ec2
KonstantinVajra <kmkm27@mail.ru>
2024-11-02 01:52:26 +0300

    add const display

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from turtledemo.nim import SCREENWIDTH, SCREENHEIGHT
+
 import pygame
+from pygame.examples.aliens import SCREENRECT
 
 pygame.init()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра ТИР')
+
+icon = pygame.image.load()
+
 
 running = True
 while running:
